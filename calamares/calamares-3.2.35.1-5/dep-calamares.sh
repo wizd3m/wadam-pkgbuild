6337ace27def57c8d0445dad0c97414d0fda0ebf
Gerard Aakders <linuxwizd3m@gmail.com>
2021-01-27 13:44:29 +0100

    removed old version

--- calamares/calamares-3.2.35.1-5/dep-calamares.sh
+++ calamares/calamares-3.2.35.1-5/dep-calamares.sh
@@ -6,9 +6,7 @@ sudo pacman -S --needed --noconfirm kio
 sudo pacman -S --needed --noconfirm solid
 sudo pacman -S --needed --noconfirm yaml-cpp
 sudo pacman -S --needed --noconfirm kpmcore
-yay -S mkinitcpio-openswap --noconfirm
 sudo pacman -S --needed --noconfirm boost-libs
-yay -S ckbcomp --noconfirm
 sudo pacman -S --needed --noconfirm hwinfo
 sudo pacman -S --needed --noconfirm qt5-svg
 sudo pacman -S --needed --noconfirm polkit-qt5
@@ -16,9 +14,12 @@ sudo pacman -S --needed --noconfirm gtk-update-icon-cache
 sudo pacman -S --needed --noconfirm plasma-framework
 sudo pacman -S --needed --noconfirm qt5-xmlpatterns
 sudo pacman -S --needed --noconfirm squashfs-tools
-
 sudo pacman -S --needed --noconfirm extra-cmake-modules
 sudo pacman -S --needed --noconfirm qt5-tools
 sudo pacman -S --needed --noconfirm qt5-translations
 sudo pacman -S --needed --noconfirm git
 sudo pacman -S --needed --noconfirm boost
+
+# from my repo at https://wizd3m.github.io/wadam-repo
+sudo pacman -S --needed --noconfirm mkinitcpio-openswap
+sudo pacman -S --needed --noconfirm ckbcomp
